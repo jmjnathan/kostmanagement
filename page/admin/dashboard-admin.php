93c19227bc6ed723c5457d058a42665d6c9e2bd1
Jonathan Christiawan <jmjchristnathan@gmail.com>
2024-12-08 13:31:59 +0700

    perubahan link cdn tailwind

--- page/admin/dashboard-admin.php
+++ page/admin/dashboard-admin.php
@@ -46,7 +46,7 @@
       <head>
          <meta charset="UTF-8">
          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@latest/dist/tailwind.min.css" rel="stylesheet">
+         <script src="https://cdn.tailwindcss.com"></script>        
          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
          <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
          <title>Dashboard</title>
